ff7fcaf3c366c63585e5bd0f44f380745f8b179b
Matheus Wichman <matheushw@outlook.com>
2019-10-06 21:52:56 -0300

    Styles

--- webpack.common.js
+++ webpack.common.js
@@ -9,8 +9,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       },
     ],
   },
